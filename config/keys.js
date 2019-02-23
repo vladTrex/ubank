5c5c4b38d5105dd485ad54a517851c0c03845aa7
Vladyslav Tertyshnyi <vladislafus@gmail.com>
2019-02-23 23:08:31 +0200

    add jwt creation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://vladt:qwerty123@ds251284.mlab.com:51284/v_db'
+    mongoURI: 'mongodb://vladt:qwerty123@ds251284.mlab.com:51284/v_db',
+    secretOrKey: 'secret',
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../../models/Users');
+const keys = require('../../config/keys');
 
 const router = express.Router();
 
@@ -63,7 +65,19 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if(isMatch){
-                        return res.json({message: 'Success'});
+                        const payload = {
+                            id: user.id,
+                            name: user.name,
+                            avatar: user.avatar,
+                        };
+
+                        jwt.sign(payload, keys.secretOrKey, {expiresIn: 3600}, (err, token) => {
+                            res.json({
+                                success: true,
+                                token: 'Bearer '+ token,
+                            });
+                        });
+                        // return res.json({message: 'Success'});
                     } else {
                         return res.status(400).json({password: 'Password is incorrect'});
                     }
